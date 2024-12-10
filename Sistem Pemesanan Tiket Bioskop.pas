92d831f9328f18dfd4e9995bc36f865cd9baa4c9
alyadfr <alyadefirasubiantoro@gmail.com>
2024-12-10 23:21:01 +0700

    Update Sistem Pemesanan Tiket Bioskop.pas

--- Sistem Pemesanan Tiket Bioskop.pas
+++ Sistem Pemesanan Tiket Bioskop.pas
@@ -81,6 +81,12 @@ begin
   else
     writeln('Kursi sudah dipesan. Pilih kursi lain.');
 end;
+procedure BatalkanKursi;
+var
+  baris, kolom: integer;
+begin
+  write('Masukkan nomor baris (1-', MAX_BARIS, '): '); readln(baris);
+  write('Masukkan nomor kolom (1-', MAX_KOLOM, '): '); readln(kolom);
 
 begin
   InisialisasiKursi;
